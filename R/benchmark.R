bf067cbaa057d8ce5022cf09f587308569b6a15d
Christian Panse <cp@fgcz.ethz.ch>
2021-10-19 10:11:40 +0200

    Add mzR in DESCRIPTION suggest field

--- R/benchmark.R
+++ R/benchmark.R
@@ -1,9 +1,13 @@
 #R
-#' benchmark
+#' RawFileReader_read_peaks benchmark
+#' 
+#' derives numbers to evaluate time performance for reading a single spectrum in
+#' dependency from the chunk size (how many spectra are read in one function call)
+#' for reading different numbers of overall spectra. 
 #'
-#' @param nv 
-#' @param sizev 
-#' @param rawfile 
+#' @param nv number of spectra to be read.
+#' @param sizev number of spectra write and parsed in one single junk.
+#' @param rawfile the Thermo Fisher Scientific raw file.
 #'
 #' @return \code{data.frame}
 #' @export ioBenchmark
@@ -13,11 +17,11 @@
 #' EH4547  <- normalizePath(eh[["EH4547"]])
 #' (rawfileEH4547  <- paste0(EH4547 , ".raw"))
 #' if (!file.exists(rawfileEH4547 )){
-#'   file.link(EH4547 , rawfileEH4547 
+#'   file.link(EH4547 , rawfileEH4547)
 #'   }
 #' S <- ioBenchmark(1000, c(128, 256, 128, 256), rawfile=rawfileEH4547)
-ioBenchmark <- function(nv=c(1000, 5000, 10000),
-                        sizev=c(8, 16, 32, 64, 128, 256, 8, 16, 32, 64, 128, 256, 8, 16, 32, 64,
+ioBenchmark <- function(nv = c(1000, 5000, 10000),
+                        sizev = c(8, 16, 32, 64, 128, 256, 8, 16, 32, 64, 128, 256, 8, 16, 32, 64,
                                 128, 256, 8, 16, 32, 64, 128, 256, 8, 16, 32, 64, 128, 256, 8, 16,
                                 32, 64, 128, 256), rawfile){
   
@@ -26,8 +30,7 @@ ioBenchmark <- function(nv=c(1000, 5000, 10000),
     lapply(sizev,
            function(size){
              start_time <- Sys.time()
-             rv <- MsBackendRawFileReader:::.RawFileReader_read_peaks(rawfile, 1:n,
-                                                                      maxGroupSize = size)
+             rv <- .RawFileReader_read_peaks(rawfile, 1:n, maxGroupSize = size)
              end_time <- Sys.time()
              d <- end_time - start_time
              # message(sprintf("%d - %s", size, d))
